82ae555a70f73be2ec0297d7e495310e1cc23682
Mark Plagge <plagge_mark@columbusstate.edu>
2022-02-19 20:42:26 -0700

    Update tracker and app

--- app/app.py
+++ app/app.py
@@ -17,26 +17,30 @@ def get_warning_services():
     services = d['services']
     print(d)
     print(services)
+    urls = d['service_urls']
+    
     days += random.randint(1,10)
     with open(warning_file,'w') as f:
         yaml.dump(d,f)
-    return days,services
+    return days,services,urls
 
 
 def generate_warning_services():
-    days, services = get_warning_services()
+    days, services,urls = get_warning_services()
     warning = f"{days} days since last major holodeck malfunction"
     srv_msg = []
     nl = '\n'.join(srv_msg)
     service_list = f"<ul>\n{nl}\n</ul> "
-    return warning,services
+
+
+    return warning,services,urls
 
 
 
 @app.route('/')
 def index():
-    warning, services = generate_warning_services()
-    return render_template('main.html',welcome=welcome,subtitle=subtitle,warning=warning,services=services)
+    warning, services, urls = generate_warning_services()
+    return render_template('main.html',welcome=welcome,subtitle=subtitle,warning=warning,services=services,urls=urls)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=9999)
